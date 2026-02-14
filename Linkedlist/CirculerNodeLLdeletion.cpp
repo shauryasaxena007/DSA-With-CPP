3ff8289e04c7642be390b68d8090e502d4d0de35
Shaurya Saxena <shauryasaxena147@gmail.com>
2026-02-15 00:03:38 +0530

    deletion

--- Linkedlist/CirculerNodeLLdeletion.cpp
+++ Linkedlist/CirculerNodeLLdeletion.cpp
@@ -37,33 +37,73 @@ void insertNode(Node* &tail, int element, int d){
 }
 void print(Node* tail){
     Node* temp =tail;
+    if(tail == NULL){
+        cout<<"list is empty"<<endl;
+    }
     do{
         cout<<tail->data<<" ";
         tail= tail-> next;
     }while(tail!= temp);
     cout<<endl;
 }
+
+void Deletion(Node* &tail, int value){
+    if(tail == NULL){
+        cout<<"empty"<<endl;
+        return;
+    }
+    else{
+        Node* prev = tail;
+        Node* curr = prev-> next;
+
+        while(curr->data != value){
+            prev=curr;
+            curr = curr -> next;
+        }
+        prev->next = curr-> next;
+
+        //1 node
+        if(curr == prev){
+            tail= NULL;
+        }
+
+        //for 2 node
+        else if(tail == curr){
+            tail = prev;
+        }
+        curr -> next =NULL;
+        delete curr;
+    }
+}
 int main(){
     Node* tail =NULL;
 
-    insertNode(tail, 5,3);
-    print(tail);
-
-    insertNode(tail, 3,2);
-    print(tail);
-
-    insertNode(tail, 2,13);
-    print(tail);
-
-    insertNode(tail, 13,45);
-    print(tail);
-
-    insertNode(tail, 45,23);
-    print(tail);
-
-     insertNode(tail, 2,13);
-    print(tail);
-    //   insertNode(tail, 2,12);
+    // insertNode(tail, 5,3);
     // print(tail);
+
+    // insertNode(tail, 3,2);
+    // print(tail);
+
+    // insertNode(tail, 2,13);
+    // print(tail);
+
+    // insertNode(tail, 13,45);
+    // print(tail);
+
+    // insertNode(tail, 45,23);
+    // print(tail);
+
+     insertNode(tail, 23,33);
+    print(tail);
+
+    // insertNode(tail, 33,22);
+    // print(tail);
+
+    // Deletion(tail,23);
+    // print(tail);
+
+    Deletion(tail,33);
+    print(tail);
+   
     return 0;
 }
--- Linkedlist/tempCodeRunnerFile.cpp
+++ Linkedlist/tempCodeRunnerFile.cpp
@@ -1,2 +1,15 @@
-  insertNode(tail, 3,2);
-    print(tail);
+
+    // insertNode(tail, 5,3);
+    // print(tail);
+
+    // insertNode(tail, 3,2);
+    // print(tail);
+
+    // insertNode(tail, 2,13);
+    // print(tail);
+
+    // insertNode(tail, 13,45);
+    // print(tail);
+
+    // insertNode(tail, 45,23);
+    // print(tail);
