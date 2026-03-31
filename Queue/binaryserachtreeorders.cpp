7f3777141dd7f2d564f25c8fef916733ff3b6fe1
Shaurya Saxena <shauryasaxena147@gmail.com>
2026-03-31 14:12:19 +0530

    BST deletion

--- Queue/binaryserachtreeorders.cpp
+++ Queue/binaryserachtreeorders.cpp
@@ -84,6 +84,61 @@ node* insertIntoBST(node* &root, int data){
     return root;
 }
 
+
+//minimum value in the BST
+node* minValue(node* root){
+    node* temp = root;
+    while(temp -> left != NULL){
+        temp = temp -> left;
+    }
+    return temp;
+}
+
+//maximum value in the BST
+node* maxValue(node* root){
+    node* temp = root;
+    while(temp -> right != NULL){
+        temp = temp -> right;
+    }
+    return temp;
+}
+
+node* deleteFromBST(node* &root, int data){
+    if(root == NULL){
+        return root;
+    }
+    if(data < root -> data){
+        root -> left = deleteFromBST(root -> left, data);
+    }
+    else if(data > root -> data){
+        root -> right = deleteFromBST(root -> right, data);
+    }
+    else{
+        //case 1 : no child
+        if(root -> left == NULL && root -> right == NULL){
+            delete root;
+            return NULL;
+        }
+        //case 2 : one child
+        else if(root -> left != NULL && root -> right == NULL){
+            node* temp = root -> left;
+            delete root;
+            return temp;
+        }
+        else if(root -> left == NULL && root -> right != NULL){
+            node* temp = root -> right;
+            delete root;
+            return temp;
+        }
+        //case 3 : two children
+        else{
+            int mini = minValue(root -> right) -> data;
+            root -> data = mini;
+            root -> right = deleteFromBST(root -> right, mini);
+            return root;
+        }
+    }
+}
 void takeInput(node* &root){
     int data;
      cin>>data;
@@ -110,5 +165,7 @@ int main(){
     cout<<" \n printing the postorder traversal output "<<endl;
     postorder(root);
 
+    cout<<" \n minimum value in the BST is "<<minValue(root) -> data<<endl;
+    cout<<" \n maximum value in the BST is "<<maxValue(root) -> data<<endl;
     return 0;
 }
