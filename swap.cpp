8cafe042c5a886b782c5b88f9760180d0f68e245
Shaurya Saxena <shauryasaxena147@gmail.com>
2026-05-12 15:18:14 +0530

    sap

--- swap.cpp
+++ swap.cpp
@@ -7,7 +7,7 @@ int main(){
     // swap(a,b);
     // cout<<"After swapping a is "<<a<<" and b is "<<b<<endl; 
     // return 0;
-
+//swap function is not working because it is pass by value and it is not changing the value of a and b in main function
     int temp;
     //temp is hold one value in swapping procee
     cout<<"Before swapping a is "<<a<<" and b is "<<b<<endl;
