0d76cced2375396e3f71780d95de3f425f1d648c
Shaurya Saxena <shauryasaxena147@gmail.com>
2026-05-28 20:57:06 +0530

    lefttringlePattern

--- 32leftTrianglePat.cpp
+++ 32leftTrianglePat.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main(){
     int n;
-    cout<<"Enter the size of the square pattern: ";
+    cout<<"Enter the size of the left triangle pattern: ";
     cin>>n;
     for(int i=1; i<=n; i++){
-        for(int j=1; j<=i7; j++){
+        for(int j=1; j<=i; j++){
             cout<<"* ";
         }
         cout<<endl;
