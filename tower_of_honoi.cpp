ab2c0d8f483de2ec3e518ebd9151723ee2380525
Shaurya Saxena <shauryasaxena147@gmail.com>
2026-07-05 23:43:44 +0530

    honoi tower

--- tower_of_honoi.cpp
+++ tower_of_honoi.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 void towerOfHanoi(int n, char source, char destination, char auxiliary) {
-    if (n ==1) {
+    if (n == 1) {
         cout << "Move disk 1 from " << source << " to " << destination << endl;
         return;
     }
