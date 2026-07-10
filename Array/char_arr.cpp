9b790f5abedd4df11a7020c429b4a38e647c8b9f
Shaurya Saxena <shauryasaxena147@gmail.com>
2026-07-10 23:50:04 +0530

    charARRAY

---
+++ Array/char_arr.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Enter your name: ";
+    char name[100];
+    cin.getline(name,100);
+    cout<<"Hello, "<<name<<"!"<<endl;
+    return 0;
+    
+}
