0fa77d2a1bc75ac6e3bba0fbeba0e515dd1256e0
Shaurya Saxena <shauryasaxena147@gmail.com>
2026-07-11 15:43:33 +0530

    concatenate

---
+++ Array/concatatnate.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string str1,str2;
+    cout<<"Enter first string: ";
+    cin>>str1;
+    cout<<"Enter second string: ";
+    cin>>str2;
+  
+    cout<<"Concatenated string is: "<<str1<<" "<< str2<<endl;
+    return 0;
+
+}
