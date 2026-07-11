5f7467cfb233a99e43079f19e651ac4c2df1867d
Shaurya Saxena <shauryasaxena147@gmail.com>
2026-07-11 15:40:45 +0530

    lenstr

---
+++ Array/len_string.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+ string str;
+ cout<<"Enter a string: ";
+    cin>>str;
+    int len=str.length();
+    cout<<"Length of the string is: "<<len<<endl;
+    return 0;
+}
