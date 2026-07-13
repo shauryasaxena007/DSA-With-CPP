ac71326bda2e0e2c979e62ce38f32c509f8d51eb
Shaurya Saxena <shauryasaxena147@gmail.com>
2026-07-14 00:13:37 +0530

    capital first

--- Array/capitalize+first.cpp
+++ Array/capitalize+first.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 void capitalize(string &str){
  bool newWord = true;
- for(char
+ for(char &c : str){
+        if(newWord && isalpha(c)){
+            c = toupper(c);
+            newWord = false;
+        } else if(c == ' '){
+            newWord = true;
+        }
+    }
+}
+int main(){
+    string str;
+    cout<<"Enter a string: ";
+    getline(cin, str);
+    capitalize(str);
+    cout<<"String after capitalizing first letter of each word: "<<str<<endl;
+    return 0;
 }
